f768b4f49ddeafcbb8a8ddcc184fa99199973a11
ItzDeme <61122692+ItzDeme@users.noreply.github.com>
2023-07-16 14:59:59 -0400

    Added more logic and copy to clipboard.
    Added raid size choice (10m/25m)
    Added raid difficulty choice (norm/heroic)
    Removed the "," for the last class (spec)
    Put "/" between each spec, but not at the end of the spec (holy/ret/prot) rather than (holy/ret/prot/)
    Added a "copy to clipboard" button

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import { Container, Row, Col, Button } from 'react-bootstrap';
 
 function App() {
   const [chosenRaid, setChosenRaid] = useState("");
+  const [chosenRaidAmount, setChosenRaidAmount] = useState("10m");
+  const [chosenRaidDifficulty, setChosenRaidDifficulty] = useState("Norm");
   const [neededClasses, setNeededClasses] = useState({
     Warr: ["Prot", "DPS"], 
         Lock: ["DPS"],
@@ -21,28 +23,47 @@ function App() {
         Dk: ["Tank", "DPS"]
   });
 
+  const copyText = () =>{
+    const text = document.querySelector('.copyThis').innerHTML
+    navigator.clipboard.writeText(text);
+  }
+
   return (
     <div>
       <Topper />
       <Row>
         <Col>
-          <Raid chosenRaid={chosenRaid} setChosenRaid={setChosenRaid} />
+          <Raid chosenRaid={chosenRaid} 
+                setChosenRaid={setChosenRaid} 
+                chosenRaidAmount={chosenRaidAmount} 
+                setChosenRaidAmount={setChosenRaidAmount}
+                chosenRaidDifficulty={chosenRaidDifficulty} 
+                setChosenRaidDifficulty={setChosenRaidDifficulty}/>
         </Col>
         <Col>
           <Classes neededClasses={neededClasses} setNeededClasses={setNeededClasses} />
         </Col>
       </Row>
       <div>
-        <h1>LFM {chosenRaid} {Object.keys(neededClasses).map((classSpecs) => {
+        <h1 className='copyThis'>LFM {chosenRaid} {'[' + chosenRaidAmount + ']'} {'[' + chosenRaidDifficulty + ']'} 
+        {Object.keys(neededClasses).map((classSpecs) => {
 
-          return neededClasses[classSpecs].length < 1 ? '' : [
-            classSpecs, " (", neededClasses[classSpecs].map((classSpec) => {
-              return (classSpec)
+          return neededClasses[classSpecs].length < 1 ? '' : 
+          //checks if it's the first in the object
+          [ neededClasses[classSpecs].length  < 1 
+          //checks if the class/spec before it has anything, it will not put comma is it doesnt.
+          // || Object.keys(neededClasses)[Object.keys(neededClasses).length - Object.keys(neededClasses).length] !== classSpecs ? (neededClasses[Object.keys(neededClasses)[(Object.keys(neededClasses).indexOf(classSpecs)-1)]].length < 1 ? '' : ',') : false //console.log(Object.keys(neededClasses)[Object.keys(neededClasses).length - Object.keys(neededClasses).length])
+          // //Object.keys(neededClasses)[(Object.keys(neededClasses).indexOf(classSpecs)-1) === Object.keys(neededClasses)[Object.keys(neededClasses).length - Object.keys(neededClasses).length]] ? 'true' : "false"
+          || Object.keys(neededClasses)[Object.keys(neededClasses).length - Object.keys(neededClasses).length] === classSpecs ? "" : ", ",
+            
+          classSpecs, " (", neededClasses[classSpecs].map((classSpec) => {
+              return [classSpec, neededClasses[classSpecs][neededClasses[classSpecs].length -1] === classSpec ? "" : "/"]
             })
-            , "), "]
+            , ")" ]
         })}
 
         </h1>
+        <Button onClick={()=>{copyText()}}>Copy to Clipboard</Button>
       </div>
     </div>
   );
--- src/components/Raid.js
+++ src/components/Raid.js
@@ -2,7 +2,12 @@ import React, { useState } from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import './Raid.css';
 
-function Raid({ chosenRaid, setChosenRaid }) {
+function Raid({ chosenRaid, 
+                setChosenRaid,
+                chosenRaidDifficulty, 
+                setChosenRaidDifficulty, 
+                chosenRaidAmount, 
+                setChosenRaidAmount}) {
     let raids = [
         "Naxx",
         "EoE",
@@ -15,8 +20,17 @@ function Raid({ chosenRaid, setChosenRaid }) {
         // "The Ruby Sanctum"
     ]
 
+    let raidAmount = ["10m", "25m"]
+    let raidDifficulty = ["Norm", "Heroic"]
+
     const getRaid = (e) => {
         setChosenRaid(e.target.value);
+    }
+    const getRaidAmount = (e) => {
+        setChosenRaidAmount(e.target.value);
+    }
+    const getRaidDifficulty = (e) => {
+        setChosenRaidDifficulty(e.target.value);
     }
 
     return (
@@ -34,6 +48,42 @@ function Raid({ chosenRaid, setChosenRaid }) {
                                 value={raid}
                                 key={raid}>
                                 {raid}
+                            </Button>
+                        })
+                    }
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                <h5>Amount</h5>
+                    {
+                        raidAmount.map((amount) => {
+                            return <Button onClick={(e) => { getRaidAmount(e) }}
+                                size='md'
+                                className='raidButtons'
+                                variant={
+                                    amount === chosenRaidAmount ? 'success' : 'outline-dark'
+                                }
+                                value={amount}
+                                key={amount}>
+                                {amount}
+                            </Button>
+                        })
+                    }
+                </Col>
+                <Col>
+                <h5>Difficulty</h5>
+                    {
+                        raidDifficulty.map((difficulty) => {
+                            return <Button onClick={(e) => { getRaidDifficulty(e) }}
+                                size='md'
+                                className='raidButtons'
+                                variant={
+                                    difficulty === chosenRaidDifficulty ? 'success' : 'outline-dark'
+                                }
+                                value={difficulty}
+                                key={difficulty}>
+                                {difficulty}
                             </Button>
                         })
                     }
